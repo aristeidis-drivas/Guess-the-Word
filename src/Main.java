ae8e7ac5cae0979d2240e7acf9275dc0a1d95704
Aristeidis Drivas <aristeidisdrivas@gmail.com>
2022-03-12 18:16:22 +0200

    added option for 4-5 letter word + randomgen word picker

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,29 @@ public class Main {
     public static void main(String[] args) {
         int counter = 0;
         int tries = 3;
-        String keyword = "HOME";
-        char [] word = keyword.toCharArray();
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Guess the 4-letter word!");
+        Scanner myWord = new Scanner(System.in);
+        System.out.println("Welcome to the Guess the Word Game!");
+        System.out.println("Press 4 to guess a 4-letter word or 5 for a 5-letter word.");
+        int pick = myObj.nextInt();
+        String randomWord = keyword(pick);
+        while (randomWord.equals("null")) {
+            pick = myObj.nextInt();
+            randomWord = keyword(pick);
+        }
+        System.out.println(randomWord);
+        char [] word = randomWord.toCharArray();
         System.out.println("You have " + tries + " tries.");
 
-        while (counter < 3) {
-            String guess = myObj.nextLine();
+        while (counter < tries) {
+            String guess = myWord.nextLine();
             String uppercaseGuess = guess.toUpperCase();
             char [] guessWord = uppercaseGuess.toCharArray();
             System.out.println("You typed:");
             for (int i = 0; i< guessWord.length; i++) {
                 System.out.println("--> " + guessWord[i]);
             }
-            if (uppercaseGuess.equals(keyword)) {
+            if (uppercaseGuess.equals(randomWord)) {
                 System.out.println("Congratulations! "+ uppercaseGuess + " is the correct word!");
                 return;
             }
@@ -33,5 +41,24 @@ public class Main {
             System.out.println((tries-counter) + " tries/try remaining.");
         }
         System.out.println("You ran out of tries. Game over!");
+    }
+
+    public static String keyword(int numberOfLetters) {
+        Scanner scanner = new Scanner(System.in);
+        String word;
+        switch (numberOfLetters) {
+            case 4:
+                String [] fourLetterWord = {"AREA", "BABY", "BALL", "BODY", "CARD", "DESK", "FORK", "MENU", "SAND", "WARD"};
+                word = fourLetterWord[(int) (Math.random()*10)];
+                break;
+            case 5:
+                String [] fiveLetterWord = {"ADULT", "APPLE", "BEACH", "CREAM", "FLASH", "GRASS", "HOUSE", "METAL", "SCORE", "TRACK"};
+                word = fiveLetterWord[(int) (Math.random()*10)];
+                break;
+            default:
+                System.out.println("Wrong number. Choose between 4-5.");
+                word = "null";
+        }
+        return word;
     }
 }
