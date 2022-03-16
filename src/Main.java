4e2bd7790a65f4c384185dfa0a926ce1253e55db
Aristeidis Drivas <aristeidisdrivas@gmail.com>
2022-03-16 14:51:32 +0200

    added play again option

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,9 @@ public class Main {
         Scanner myObj = new Scanner(System.in);
         Scanner myWord = new Scanner(System.in);
         Scanner myTry = new Scanner(System.in);
+        Scanner myBtn = new Scanner(System.in);
         Scanner txtDb = new Scanner(new File("words.txt"));
+
         ArrayList<String> threeLetterList = new ArrayList<String>();
         ArrayList<String> fourLetterList = new ArrayList<String>();
         ArrayList<String> fiveLetterList = new ArrayList<String>();
@@ -39,72 +41,72 @@ public class Main {
         numberListMapping.put(4, fourLetterList);
         numberListMapping.put(5, fiveLetterList);
         numberListMapping.put(6, sixLetterList);
+        boolean gameOn = true;
 
-        System.out.println("Welcome to the Guess the Word Game!");
-        System.out.println("Press 3 to 6 to select how many letters the secret word contains");
-        int pick = myObj.nextInt();
-        while (pick != 3 && pick != 4 && pick != 5 && pick != 6) {
-            System.out.println("Wrong number! Choose between 3 and 6.\"");
-            pick = myObj.nextInt();
-        }
-
-        Random randomizer = new Random();
-        String random = numberListMapping.get(pick).get(randomizer.nextInt(numberListMapping.get(pick).size()));
-        System.out.println(random);
-        char[] word = random.toCharArray();
-
-        int counter = 0;
-        System.out.println("How many tries do you want to have?");
-        int tries = myTry.nextInt();
-        System.out.println("You have " + tries + " tries.");
-
-        while (counter < tries) {
-            String guess = myWord.nextLine();
-            String uppercaseGuess = guess.toUpperCase();
-            char[] guessWord = uppercaseGuess.toCharArray();
-            System.out.println("You typed:");
-            for (int i = 0; i < guessWord.length; i++) {
-                System.out.println("--> " + guessWord[i]);
+        while (gameOn) {
+            System.out.println("Welcome to the Guess the Word Game!");
+            System.out.println("Press 3 to 6 to select how many letters the secret word contains");
+            int pick = myObj.nextInt();
+            while (pick != 3 && pick != 4 && pick != 5 && pick != 6) {
+                System.out.println("Wrong number! Choose between 3 and 6.\"");
+                pick = myObj.nextInt();
             }
-            while (guessWord.length != pick || !numberListMapping.get(pick).contains(uppercaseGuess)) {
-                System.out.println("The word must have " + pick + " letters or the word is invalid. Type again: ");
-                guess = myWord.nextLine();
-                uppercaseGuess = guess.toUpperCase();
-                guessWord = uppercaseGuess.toCharArray();
-                System.out.println("You typed:");
 
+            Random randomizer = new Random();
+            String random = numberListMapping.get(pick).get(randomizer.nextInt(numberListMapping.get(pick).size()));
+            System.out.println(random);
+            char[] word = random.toCharArray();
+
+            int counter = 0;
+            System.out.println("How many tries do you want to have?");
+            int tries = myTry.nextInt();
+            System.out.println("You have " + tries + " tries.");
+
+            while (counter < tries) {
+                String guess = myWord.nextLine();
+                String uppercaseGuess = guess.toUpperCase();
+                char[] guessWord = uppercaseGuess.toCharArray();
+                System.out.println("You typed:");
                 for (int i = 0; i < guessWord.length; i++) {
                     System.out.println("--> " + guessWord[i]);
                 }
-            }
 
-            /*while (!numberListMapping.get(pick).contains(uppercaseGuess)) {
-                System.out.println("This is not a valid word! Type again: ");
-                guess = myWord.nextLine();
-                uppercaseGuess = guess.toUpperCase();
-                guessWord = uppercaseGuess.toCharArray();
-                System.out.println("You typed:");
+                while (guessWord.length != pick || !numberListMapping.get(pick).contains(uppercaseGuess)) {
+                    System.out.println("The word must have " + pick + " letters or the word is invalid. Type again: ");
+                    guess = myWord.nextLine();
+                    uppercaseGuess = guess.toUpperCase();
+                    guessWord = uppercaseGuess.toCharArray();
+                    System.out.println("You typed:");
+
+                    for (int i = 0; i < guessWord.length; i++) {
+                        System.out.println("--> " + guessWord[i]);
+                    }
+                }
+
+                if (uppercaseGuess.equals(random)) {
+                    System.out.println("Congratulations! " + uppercaseGuess + " is the correct word!");
+                    System.out.println("Play again? 1 for yes, 2 for no.");
+                    int playAgain = myBtn.nextInt();
+                    if (playAgain == 1) break;
+                    else return;
+                }
 
                 for (int i = 0; i < guessWord.length; i++) {
-                    System.out.println("--> " + guessWord[i]);
+                    for (int j = 0; j < word.length; j++) {
+                        if (guessWord[i] == (word[j]) && i == j)
+                            System.out.println(guessWord[i] + " exists in the word and is in the correct place!");
+                        else if (guessWord[i] == (word[j]) && i != j)
+                            System.out.println(guessWord[i] + " exists in the word but is in the wrong place.");
+                    }
                 }
-            }*/
-
-            if (uppercaseGuess.equals(random)) {
-                System.out.println("Congratulations! " + uppercaseGuess + " is the correct word!");
-                return;
+                counter++;
+                System.out.println((tries - counter) + " tries/try remaining.");
             }
-            for (int i = 0; i < guessWord.length; i++) {
-                for (int j = 0; j < word.length; j++) {
-                    if (guessWord[i] == (word[j]) && i == j)
-                        System.out.println(guessWord[i] + " exists in the word and is in the correct place!");
-                    else if (guessWord[i] == (word[j]) && i != j)
-                        System.out.println(guessWord[i] + " exists in the word but is in the wrong place.");
-                }
-            }
-            counter++;
-            System.out.println((tries - counter) + " tries/try remaining.");
+            if (tries-counter == 0) System.out.println("You ran out of tries. Game over!");
+            System.out.println("Play again? 1 for yes, 2 for no.");
+            int playAgain = myBtn.nextInt();
+            if (playAgain == 1) continue;
+            else return;
         }
-        System.out.println("You ran out of tries. Game over!");
     }
 }
